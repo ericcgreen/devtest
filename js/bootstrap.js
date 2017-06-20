2b0613f2e790b0efbe97f6da16b3174ee154c4f6
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-06-20 01:30:08 -0400

    added some jquery

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
+// Start when document will be ready.
+$("#submit-btn").click(function() {
+var validRegExp = /^[^@]+@[^@]+.[a-z]{2,}$/i;
+var email = $("#inputEmail").val(); // Store E-mail input value in the variable email.
+var zipcode = $("#inputZipcode").val(); // Store Password input value in the variable password.
+/* Check if email has regular expressions and if not, shake */
+if (email.search(validRegExp) == -1) {
+  $("#message").html("i find your lack of faith disturbing.");
+  $("input").val("");
+}
 
+else {
+  $("#message").html("together we can rule the galaxy!!!");
+  $("input").val("");
+}
+});
+});
